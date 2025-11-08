d16d871197c68dbf33c40eb0b25d9bd468d44535
Dumb Bird <72983221+ZackeryRSmith@users.noreply.github.com>
2025-11-07 19:46:40 -0500

    fix: infinite recursion bug

--- extensions/scenes/monster_directional_sfx.gd
+++ extensions/scenes/monster_directional_sfx.gd
@@ -3,6 +3,6 @@ extends "res://scenes/monster_directional_sfx.gd"
 @onready var CC = ModLoader.get_node("ZackeryRSmith-CustomChallenges")
 
 func play_sound() -> void:
-	if CC.challenges.get("deadly_quiet"):
+	if CC.challenges.get("deadly_quiet").enabled:
 		return
 	super()
